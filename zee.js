210fb2c88e44fb90cce7d1bc5e9179ced09a18bc
Zihao Li <Zihao Li>
2017-04-28 09:28:34 -0700

    created code for dynamically adding iframe

---
+++ zee.js
@@ -0,0 +1,10 @@
+$("#button").on("click",function(){
+  console.log("clicked");
+  $("#lyrics").empty();
+  var song = "migos"; 
+  var src = "http://www.lyrics.com/lyrics/"+song;
+  $('<iframe>').attr("src", src).attr("height",300).attr("width",300).appendTo('#lyrics');
+  
+ 
+
+});
